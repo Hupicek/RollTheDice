b069edef5a1a69518321ee75577acee9f68c982b
hupic <hupic@DESKTOP-K5A3P7R>
2022-11-06 19:39:53 +0100

    Přidejte soubory projektu.

---
+++ RollTheDice/Game.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollTheDice
+{
+    public class Game
+    {
+        #region Vlastnosti
+        public int Score { get; set; }
+        public int Lives { get; set; }
+        public int CurrentNumber { get; set; }
+        #endregion
+
+        //generátor náhodných čísel
+        private Random generator = new Random();
+
+        #region Metody
+        //konstruktor (metoda, která se zavolá při vytvoření)
+        public Game()
+        {
+            Lives = 5;
+            Score = 0;
+        }
+        //
+
+        //metoda, která vygeneruje náhodné číslo na kostce
+        public void GenerateNumber()
+        {
+            int number = generator.Next(1, 7);
+            CurrentNumber = number;
+        }
+
+        public bool CheckIfWin(int chosenNumber)
+        {
+            if (chosenNumber == CurrentNumber)
+            {
+                Score++;
+                return true;
+            }
+            else
+            {
+
+
+
+                Lives--;
+
+
+
+                if (Lives == 0)
+                {
+                    Lives = 5;
+                    Score = 0;
+                }
+                return false;
+            }
+        }
+        //metoda, pro restartování počátečního stavu hry
+        public void ResetGame()
+        {
+            Score = 0;
+            Lives = 5;
+
+
+        }
+        #endregion
+    }
+}
---
+++ RollTheDice/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace RollTheDice
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        //vytvoření objektu třídy Game (soubor Game.cs), v níž je logika aplikace
+        public Game game = new Game();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            int userNumber = 0;
+            game.GenerateNumber();
+            try
+            {
+                userNumber = Convert.ToInt32(tbxNumberInput.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Invalid input, please try again (numbers 1 - 6)");
+            }
+            if (game.CheckIfWin(userNumber))
+            {
+                ChangeImage(game.CurrentNumber);
+                MessageBox.Show("You win!");
+                score.Text = Convert.ToString(game.Score);
+                lives.Text = Convert.ToString(game.Lives);
+            }
+            else
+            {
+                ChangeImage(game.CurrentNumber);
+                MessageBox.Show("Try again :)");
+                lives.Text = Convert.ToString(game.Lives);
+                score.Text = Convert.ToString(game.Score);
+            }
+            tbxNumberInput.Text = "";
+
+            //resetování obrázku na původní
+            imgDice.Source = new BitmapImage(new Uri(@$"Images/dice_default.png", UriKind.Relative));
+        }
+        public void ChangeImage(int diceNumber)
+        {
+            imgDice.Source = new BitmapImage(new Uri(@$"Images/dice{diceNumber}.png", UriKind.Relative));
+            //díky interpolaci řetěžců (znak dolaru) můžeme vložit hozené číslo do názvu souboru
+            // -> tím se při hodu čísla 3 vytvoří adresa "Images/dice3.png"
+            //    která odkazuje na obrázek s hozeným číslem 3 na hrací kostce ve složce Images
+        }
+
+        private void btnRestart_Click(object sender, RoutedEventArgs e)
+        {
+            game.Score = 0;
+            game.Lives = 5;
+
+            lives.Text = Convert.ToString(game.Lives);
+            score.Text = Convert.ToString(game.Score);
+        }
+    }
+}
